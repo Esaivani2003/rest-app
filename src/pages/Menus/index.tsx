b8fde1b7a239a2d5656cc50231d67f8d9af1a513
Esaivani2003 <sesai1906@gmail.com>
2025-03-19 12:48:50 +0530

    updates on layouts

--- src/pages/Menus/index.tsx
+++ src/pages/Menus/index.tsx
@@ -1,9 +1,12 @@
 import ProductPage from "@/components/MenuPage";
+import Layout from "@/components/layout";
 
 export default function Product() {
   return (
+    <Layout>
     <div>
       <ProductPage />
     </div>
+    </Layout>
   );
 }
