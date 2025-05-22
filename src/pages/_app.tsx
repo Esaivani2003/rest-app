9f78364b10475b3188c71a68d3993f8e24223549
Esaivani2003 <sesai1906@gmail.com>
2025-05-22 18:32:31 +0530

    alert to toast

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,21 +1,23 @@
-
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import { Toaster } from "sonner";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <div className="flex flex-col min-h-screen">
+      {/* Sonner Toaster (Global Notification Provider) */}
+      <Toaster position="top-right" richColors />
+
       {/* Navbar Always on Top */}
-      
+      {/* Add your Navbar here if needed */}
 
       {/* Main Content */}
       <main className="flex-grow">
         <Component {...pageProps} />
-        
       </main>
 
       {/* Footer Always at Bottom */}
-      
-    </div>
-  );
+      {/* Add your Footer here if needed */}
+    </div>
+  );
 }
